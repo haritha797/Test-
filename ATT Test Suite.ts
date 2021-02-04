e767d8d38fa570a9da2cf9ac81e9a2603e86b328
haritha797 <78566373+haritha797@users.noreply.github.com>
2021-02-04 17:21:20 -0500

    initial commit

---
+++ ATT Test Suite.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ATT Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>aac455e7-7fcd-415e-afac-850b250b4ed3</testSuiteGuid>
+   <testCaseLink>
+      <guid>d86423f0-cf7b-4b4f-afdb-bcaaf6731931</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Att home page</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2f915b41-d28e-40d0-8e75-26d58201a765</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Att-Deals page</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
